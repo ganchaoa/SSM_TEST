436a43f45c9896e78b15b9a5b5f10e9e0b809a78
ganchaoa <gc907308793@outlook.com>
2018-03-10 22:15:00 +0800

    添加角色

---
+++ SSM_TEST/src/main/java/com/gan/ssm/entity/Role.java
@@ -0,0 +1,32 @@
+package com.gan.ssm.entity;
+
+/**
+ * 角色
+ * @author g9073
+ *
+ */
+public class Role {
+	
+	private int id;
+	
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
